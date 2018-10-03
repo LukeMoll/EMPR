0f8a2f63a4e375561231269f6aa1e66c374060f3
Luke Moll <lm1674@york.ac.uk>
2018-10-03 09:36:37 +0100

    Moved LED pin definitions to pins.h

--- src/miniproject1_stage2.c
+++ src/miniproject1_stage2.c
@@ -1,13 +1,6 @@
 #include "lpc17xx_gpio.h"
 
-/**
- * Port and pin numbers of the LPC17xx that correspond to the builtin LEDs on the ARM mbed board.
- */ 
-#define LED_PORT 1
-#define LED0_PIN 18
-#define LED1_PIN 20
-#define LED2_PIN 21
-#define LED3_PIN 23
+#include "pins.h"
 
 /**
  * For use with GPIO_SetDir
---
+++ src/pins.h
@@ -0,0 +1,8 @@
+/**
+ * Port and pin numbers of the LPC17xx that correspond to the builtin LEDs on the ARM mbed board.
+ */ 
+#define LED_PORT 1
+#define LED0_PIN 18
+#define LED1_PIN 20
+#define LED2_PIN 21
+#define LED3_PIN 23
