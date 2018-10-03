65b43bf16a6c9334945d930a92c382e693bf49c2
Luke Moll <lm1674@york.ac.uk>
2018-10-03 12:11:10 +0100

    finished stage4 (sans binary), added -serial.o to MakefileParam ~CW

---
+++ src/miniproject2_stage4.c
@@ -0,0 +1,55 @@
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_systick.h"
+#include "pins.h"
+
+#include "status.h"
+#include "serial.h"
+#include <stdio.h>
+
+/**
+ * For use with GPIO_SetDir
+ */ 
+#define DIR_IN 0
+#define DIR_OUT 1
+
+void SysTick_Handler(void);
+
+int main(void) {
+    uint32_t led_mask = 0 | (1<<LED0_PIN) | (1<<LED1_PIN) | (1<<LED2_PIN) | (1<<LED3_PIN);
+    GPIO_SetDir(LED_PORT, led_mask, DIR_OUT);
+
+    serial_init(); //initialises to Baudrate 9600, 8 data bits (screen /dev/ttyACM0 9600,cs8)
+    write_usb_serial_blocking("Starting Count\n\r", 17);
+
+    SYSTICK_InternalInit(100);
+    SYSTICK_IntCmd(ENABLE);
+    SYSTICK_Cmd(ENABLE);
+
+    while(1);
+    return 1;
+}
+
+uint8_t counter = 0;
+uint8_t i = 0;
+
+void SysTick_Handler(void) {
+    SYSTICK_ClearCounterFlag();
+    counter++;
+    if(counter >= 10) { // in case we miss an iteration or something
+        counter = 0; // reset counter
+        if(i < 16) {
+            status_code(i);
+            char buf[12];
+            sprintf(buf, "%02d\t%01x\t0000\n\r", i, i);
+            write_usb_serial_blocking(buf, 12);
+            i++;
+        }
+        else {
+            status_code(0);
+            SYSTICK_IntCmd(DISABLE);
+            SYSTICK_Cmd(DISABLE);
+            write_usb_serial_blocking("finished count\n\r", 17);
+        }
+        
+    }
+}
---
+++ src/serial.h
@@ -0,0 +1,7 @@
+// Serial code include file
+int read_usb_serial_none_blocking(char *buf,int length);
+
+int write_usb_serial_blocking(char *buf,int length);
+
+void serial_init(void);
+
