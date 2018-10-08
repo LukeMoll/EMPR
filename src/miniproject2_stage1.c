dd6f821229d0d1adc91c4d27e68e3c7b5b2d887b
Luke Moll <lm1674@york.ac.uk>
2018-10-08 12:04:47 +0100

    Added miniproject 2 stage 1 skeleton (not working)

---
+++ src/miniproject2_stage1.c
@@ -0,0 +1,46 @@
+#include <lpc17xx_i2c.h>
+#include <lpc17xx_pinsel.h>
+#include "status.h"
+
+#define I2C_CLOCKRATE_HZ 100000
+
+int main(void);
+
+int main(void) {
+    // init I2C pinmode
+    PINSEL_CFG_Type PinCfg;
+    
+    PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+    PinCfg.Pinmode = PINSEL_PINMODE_PULLUP;
+    PinCfg.Funcnum = 3;
+    PinCfg.Portnum = 0;
+    PinCfg.Pinnum = 0; // SDA
+    PINSEL_ConfigPin(&PinCfg);
+    PinCfg.Pinnum = 1; // SCL
+    PINSEL_ConfigPin(&PinCfg);
+
+
+    I2C_Init(LPC_I2C1, I2C_CLOCKRATE_HZ);
+    I2C_IntCmd(LPC_I2C1, ENABLE);
+
+    I2C_M_SETUP_Type transferCfg;
+    transferCfg.sl_addr7bit = 0x20;
+
+    transferCfg.tx_data = NULL;
+    transferCfg.tx_length = sizeof(transferCfg.tx_data);
+
+    transferCfg.rx_data = NULL;
+    transferCfg.rx_length = 0;
+
+    transferCfg.retransmissions_max = 2; // might be useful
+
+    status_code(1);
+    if (I2C_MasterTransferData(LPC_I2C1, &transferCfg, I2C_TRANSFER_POLLING) == ERROR){
+        status_code(2);
+    }
+    else {
+        status_code(0);
+    }
+
+    return 0;
+}
