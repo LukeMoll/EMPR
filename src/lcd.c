303277cc34e554a4e9b9fab278b6c701a370bc37
FromAnkyra <cw1647@york.ac.uk>
2018-10-11 11:57:30 +0100

    modularised, modified clear_screen, attempted newline. Untested.

--- src/lcd.c
+++ src/lcd.c
@@ -6,6 +6,7 @@
 int main(void);
 void busy_wait(uint8_t duration);
 void lcd_init(); // TODO: remove and put in header file ready for modularisation
+void lcd_clear_screen();
 
 #define LCD_I2C_ADDR 0x3b
 
@@ -15,25 +16,22 @@ int main(void) {
     i2c_setup_polling();
     lcd_init();
 
-    uint8_t chars[] = { // "Hello World!"
-        0xC8,
-        0x65,
-        0x6C,
-        0x6C,
-        0x6F,
-        0x91,
-        0xD7,
-        0x6F,
-        0x72,
-        0x6C,
-        0x64,
-        0xA1
+    uint8_t hello_world[] = { // "Hello World!"
+        0xC8,   // H
+        0x65,   // e
+        0x6C,   // l
+        0x6C,   // l
+        0x6F,   // o
+        0x91,   // \n
+        0xD7,   // W
+        0x6F,   // o
+        0x72,   // r
+        0x6C,   // l
+        0x64,   // d
+        0xA1    // !
     };
-    uint8_t i, length = 12;
-    for(i=0; i < length; i++) {
-        i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x00,0x80 + i}, 2); // sets cursor address (80 is first char)
-        i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x40,chars[i]}, 2); // sets addresss in character set to read into cursor address
-    }
+    
+    write_to_screen(* hello_world, 12, 0x80)
 
     return 0;
 }
@@ -54,10 +52,7 @@ void lcd_init() {
         0x02  // last byte of init sequence
         }, 11); 
 
-    i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x00, 0x01}, 2);
-    // clear the screen (puts right arrow in all positions), need to wait 166 Fosc cycles.
-
-    busy_wait(1000); // can possibly be reduced, and ideally allow systick to be used for something else
+    lcd_clear_screen();
 }
 
 uint8_t waiting = 0;
@@ -82,4 +77,31 @@ void SysTick_Handler(void) {
     if((cycles * 10) > duration) {
         waiting = 0;
     }
+}
+
+void lcd_clear_screen() {
+
+    // i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x00, 0x01}, 2);
+    // // clear the screen (puts right arrow in all positions), need to wait 166 Fosc cycles.
+    // //TODO: Check if that specific command is actually necessary, or whether we can make our own clear screen of the type
+
+    // busy_wait(1000); // can possibly be reduced, and ideally allow systick to be used for something else
+
+    for(i=0x80, i<0xa0, i++) {
+    i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x00,i}, 2);
+    i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x40,0xa0}, 2); //writes a blank space at i  
+    }
+    
+}
+
+void write_to_screen(uint8_t * chars, uint8_t length, uint8_t base_address {
+
+    for(i=0; i < length; i++) {
+        if(chars[i] == 0x91) {
+            base_address = 90;
+            continue;
+        }
+        i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x00,base_address + i}, 2); // sets cursor address to base address
+        i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x40,chars[i]}, 2);
+        
 }
