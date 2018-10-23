45ad68a3b36449dbaa2cb604568e4de905f6d194
Luke Moll <lm1674@york.ac.uk>
2018-10-23 13:55:45 +0100

    Added lcd_write_byte

--- src/lcd.c
+++ src/lcd.c
@@ -40,18 +40,21 @@ void lcd_clear_screen() {
 
 void lcd_write_bytes(uint8_t * bytes, uint8_t length, uint8_t base_address) {
 
-    uint8_t i, j;
-    j = 0;
+    uint8_t i;
     for(i=0; i <= length; i++) {
-        if(bytes[i] == 0x91) {
+        if(bytes[i] == CHR_BLANK_NEWLINE) {
             base_address = 0xc0 - (i+2); //sets base address to first character on line 2 minus i
             continue;
         }
         //handles newline
         i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x00,base_address + i}, 2); // sets cursor address to base address
-        i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x40,bytes[j]}, 2);
-        j++;
+        i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x40,bytes[i]}, 2);
     }   
+}
+
+void lcd_write_byte(uint8_t byte, uint8_t base_address) {
+    i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x00,base_address}, 2); // sets cursor address to base address
+    i2c_write_multiple_bytes(LCD_I2C_ADDR, (uint8_t[]){0x40,byte}, 2);
 }
 
 #include "status.h"
@@ -67,13 +70,14 @@ uint8_t lcd_ascii_to_byte(char ascii) {
     }
     else {
         switch (ascii) {
-            case '@': return 0x80;
-            case '[': return 0x8A;
-            case ']': return 0x54;
-            case '_': return 0x5A;
-            case '|': return 0x55;
-            case '\a':return 0x4E;
-            case '\n':return 0x91;
+            case '@': return CHR_AT_SIGN;
+            case '[': return CHR_BRACKET_SQUARE_OPENING;
+            case ']': return CHR_BRACKET_SQUARE_CLOSING;
+            case '_': return CHR_BLOCK_1H;
+            case '|': return CHR_BAR_MIDDLE;
+            case '\a':return CHR_BELL;
+            case '\n':return CHR_BLANK_NEWLINE;
+            case '\0':return CHR_BLANK_UNKNOWN;
             default: 
             // return CHR_BLANK_UNKNOWN;
             return CHR_INVERTED_QUESTION_MARK;
@@ -88,7 +92,18 @@ uint8_t * lcd_a2b_in_place(char * chars, uint8_t length) {
 
     uint8_t i;
     for(i=0; i < length; i++) {
-        bytes[i] = lcd_ascii_to_byte(chars[i]);;
+        bytes[i] = lcd_ascii_to_byte(chars[i]);
     }
+    return bytes;
+}
+
+uint8_t lcd_a2b(char * chars, uint8_t length) {
+    uint8_t * bytes = malloc(length);
+
+    uint8_t i;
+    for(i=0; i < length; i++) {
+        bytes[i] = lcd_ascii_to_byte(chars[i]);
+    }
+
     return bytes;
 }
