f343ade892ae9c0ff351f274f9980c14517aed79
FromAnkyra <cw1647@york.ac.uk>
2018-11-06 15:03:22 +0000

    played around with the timers and tried to figure them out

--- src/test.c
+++ src/test.c
@@ -1,31 +1,79 @@
-#include "i2c.h"
 #include "serial.h"
-#include "expander.h"
-#include "keypad.h"
-#include "keymap.h"
-#include "lcd.h"
+// #include "status.h"
+#include <lpc17xx_systick.h>
 
 #include <stdio.h>
-#include <status.h>
+
+#include <lpc17xx_timer.h>
+#include <lpc17xx_rit.h>
+
+void RIT_IRQHandler(void);
+void SysTick_Handler(void);
+uint16_t counter;
 
 int main(void) {
-    i2c_setup_polling();
+    // i2c_setup_polling();
     serial_init();
-    lcd_init();
+    // lcd_init();
+    write_usb_serial_blocking("begin\r\n", 8);
+    counter = 0;
+    // status_code(1);
+    NVIC_EnableIRQ(RIT_IRQn);
+    RIT_Init(LPC_RIT);
+    // status_code(2);
+    RIT_TimerConfig(LPC_RIT, 1000);
+    // status_code(3);
+    while(1){};
+    return 0;
 
-    lcd_clear_screen();
-    uint8_t byte = lcd_ascii_to_byte("1");//works with offset 39, for some reason rather than 0x80
-    uint8_t byte2 = lcd_ascii_to_byte("p"); // keeps printing lowercase t, or 0x74 on offset 0, and 0x55 on offset 0x80. Both should work.¯\_(ツ)_/¯ 
-    uint8_t byte3 = lcd_ascii_to_byte("]");
-    uint8_t byte4 = 0x98;
-    lcd_write_byte(byte4, 0x80);
-    // lcd_clear_screen();
-    // lcd_write_byte(0x90, 0x81);
+    // SYSTICK_InternalInit(100);
+    // SYSTICK_IntCmd(ENABLE);
+    // SYSTICK_Cmd(ENABLE);
 
-    char buf[100];
-    uint8_t buf_length = snprintf(buf, 99, "byte: 0x%02x\r\n", byte3);
-    write_usb_serial_blocking(buf, buf_length);
+    // TIM_TIMERCFG_Type TIMCfg;
+    // TIMCfg.PrescaleOption = TIM_PRESCALE_USVAL;
+    // TIMCfg.PrescaleValue = 1000;
 
-    return;
+    // TIM_CAPTURECFG_Type CaptureCfg;
+    // CaptureCfg.CaptureChannel = 0;
+    // CaptureCfg.FallingEdge = ENABLE;
+    // CaptureCfg.IntOnCaption = DISABLE;
+    // CaptureCfg.RisingEdge = ENABLE;
+    
+    // write_usb_serial_blocking("begin\r\n", 8);
+
+    // TIM_ConfigStructInit(TIM_TIMER_MODE, &TIMCfg);
+    // TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &TIMCfg);
+    // TIM_ConfigCapture(LPC_TIM0, &CaptureCfg);
+    // TIM_Cmd(LPC_TIM0, ENABLE);
+
+    // haven't tested this yet, but I think I'm shooting in the right direction with maybe a hint of light rather than randomly in the dark
+    // while(1) {};
+
+    // return 0;
 }    
 
+
+// void SysTick_Handler(void) {
+//     SYSTICK_ClearCounterFlag();
+//     uint32_t val = TIM_GetCaptureValue(LPC_TIM0, TIM_COUNTER_INCAP0);
+//     char buf[100];
+//     write_usb_serial_blocking(buf,
+//         snprintf(buf, 99, "%ld\t%lx\r\n", val, val)
+//     );
+
+// }
+void RIT_IRQHandler(void) {
+    RIT_TimerClearCmd(LPC_RIT, 1);
+
+    if(counter==60069){
+        char buf[100];
+        write_usb_serial_blocking(buf, snprintf(buf,99, "counter = %d\r\n", counter));
+        counter = 0;
+    }
+    else {
+        counter++;
+    }
+
+    
+}
