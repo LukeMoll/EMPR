9db8b5d496b2a837ae2a669f67fcb0569df82fb6
FromAnkyra <cw1647@york.ac.uk>
2018-11-06 14:57:46 +0000

    Added frequency and amplitude modulation to mp3_s2 (~ldm)

--- src/miniproject3_stage2.c
+++ src/miniproject3_stage2.c
@@ -17,6 +17,9 @@
 #define SQR_PIN 16
 
 void init(void);
+uint16_t sine_wave(uint8_t x, uint8_t amplitudeDenom);
+double lookup_sin(double piCoeff);
+uint16_t transform10(double normalised);
 
 /* TODO:
  * - sin lookup table (~ldm)
@@ -48,29 +51,76 @@ void init(void) {
 
     PINSEL_ConfigPin(&PinCfg);
     DAC_Init(LPC_DAC);
-    
-    // TIM_TIMERCFG_Type TimCfg;
-    // TimCfg.PrescaleOption = TIM_PRESCALE_TICKVAL;
-    // TimCfg.PrescaleValue = 1000; // Timer scales to 1ms
 
-    // TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &TimCfg);
-    // TIM_Cmd(LPC_TIM0, ENABLE);
 }
 
 
 uint8_t tick = 0;
+uint16_t cycles = 0;
+
+uint8_t freq_scale_factor = 1;
+const uint8_t max_freq_scale_factor = 8;
+
+uint8_t amp_scale_denom = 1;
+const uint8_t max_amp_scale_denom = 16;
+
 void SysTick_Handler(void) {
     SYSTICK_ClearCounterFlag();
 
-    double tickPi = ((double)tick) * M_PI / 128.0;
-    // Transform tick [0,255] to be in interval [0,2pi]
-
-    uint16_t val = (uint16_t) (
-        ( sin(tickPi) + 1 ) // sin x [-1,1]; sin x + 1 -> [0,2]
-        * 511 // * 512 would cause 1+1*512 = 1024, which is out of range for 10 bits
-    ); // [0, 1022] (close enough)
-
+    uint16_t val = sine_wave((tick * freq_scale_factor) % 256, amp_scale_denom);
     DAC_UpdateValue(LPC_DAC, val);
 
+    if(tick == 0) {
+        cycles++; // cycles is every 256 ticks - 1 second ~= 4 cycles
+        if(cycles % 8 == 0) { // every 2 seconds, change the amplitude
+            amp_scale_denom = amp_scale_denom < max_amp_scale_denom ? 
+                amp_scale_denom + 1 :
+                1;
+        }
+        if(cycles >= 40) { // every 10 seconds, increase the frequency
+            cycles = 0;
+            freq_scale_factor = freq_scale_factor < max_freq_scale_factor ?
+                freq_scale_factor * 2 :
+                1;
+            amp_scale_denom = 1;
+        }
+    }
+
     tick++; // We like the overflow >:)))
+}
+
+
+/**
+ * Generates a sine wave from a 8-bit tick
+ *  tick: [0,255]
+ *  
+ *  returns: [0,1023]
+ */
+uint16_t sine_wave(uint8_t tick, uint8_t amplitudeDenom) {
+
+    return transform10(
+        lookup_sin( 
+            ((double)tick) /
+            128.0f
+        ) 
+        /
+        ((double) amplitudeDenom)
+    );
+}
+
+/**
+ * Takes radian value (DIVIDED BY PI) and returns sine of value (in interval [-1.0, 1.0])
+ */
+double lookup_sin(double piCoeff) {
+    return sin(piCoeff * M_PI);
+}
+
+/**
+ * Takes normalised value (in interval [-1.0, 1.0]) and transforms for use with 10-bit ADC.
+ */
+uint16_t transform10(double normalised) {
+    return (uint16_t) (
+        ( normalised + 1.0) // [-1,1]; -> [0,2]
+        * 511.5 // * 512 would cause 2*512 = 1024, which is out of range for 10 bits
+    ); // [0, 1023]
 }
