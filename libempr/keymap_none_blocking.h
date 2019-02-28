155c29296acb18da98cca64e5e4fe42479f71756
FromAnkyra <cw1647@york.ac.uk>
2019-02-28 10:37:42 +0000

    changed ui to use abjp's keypad library

--- libempr/keymap_none_blocking.h
+++
@@ -1,6 +0,0 @@
-#ifndef EMPR_KEYMAP
-#define EMPR_KEYMAP
-#include <stdint.h>
-char keymap_get_ascii_character(uint8_t key_pressed);
-char keypad_keys[16];
-#endif
--- libempr/keypad_none_blocking.h
+++
@@ -1,8 +0,0 @@
-#ifndef EMPR_KEYPAD
-#define EMPR_KEYPAD
-#define KEYPAD_I2C_ADDR 0x21
-#include <stdint.h>
-uint16_t keypad_read_diff(uint8_t * pressedkey, uint16_t current_state);
-uint16_t keypad_read();
-char * format_keypad_state(uint16_t state);
-#endif
