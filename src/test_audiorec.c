cad1604db6cb1793ab8f00bca6558ae6a6696e48
Luke Moll <lm1674@york.ac.uk>
2019-03-05 16:48:30 +0000

    Added record,play test (doesn't work :c)

--- src/test_audiorec.c
+++ src/test_audiorec.c
@@ -1,15 +1,19 @@
+#include <libempr/rithandler.h>
 #include <lpc17xx_rit.h>
 #include <lpc17xx_clkpwr.h> // https://youtu.be/h-mUGj41hWA
 #include <lpc17xx_gpio.h>
 #include <stdbool.h>
 #include <libempr/serial.h>
 #include <libempr/status.h>
+#include <libempr/i2c.h>
+#include <libempr/digit.h>
+#include <libempr/audioplayback.h>
 #include <libempr/adc.h>
 #include <stddef.h>
 
-uint32_t RIT_TimerConfig_us(LPC_RIT_TypeDef *RITx, uint32_t microseconds);
 
 void init();
+void arc_rithandler(void);
 
 void main(void) {
     init();
@@ -18,77 +22,52 @@ void main(void) {
 }
 
 
-#define BUF_LEN 2048
+#define BUF_LEN 0x2000
 uint16_t buf[BUF_LEN];
+uint16_t seq = 0;
 void init(){
-
+    i2c_setup_polling();
+    digit_setPower(4);
+    digit_writeTwoHexBytes(0xFFFF);
     serial_init();
     serial_puts("Hello!\r\n");
     adc_setup(ADC_PORT, ADC_PIN, ADC_FUNCNUM, ADC_CHANNEL);
 
+    RIT_setHandler(arc_rithandler);
+
     RIT_Init(LPC_RIT);
-    RIT_TimerConfig_us(LPC_RIT, 125);
+    RIT_TimerConfig_us(LPC_RIT, PLAYBACK_4KHZ);
     NVIC_EnableIRQ(RIT_IRQn);
+    status_code(1);
 }
 
-uint32_t RIT_TimerConfig_us(LPC_RIT_TypeDef *RITx, uint32_t microseconds) {
-    	uint32_t clock_rate, cmp_value;
-    	// CHECK_PARAM(PARAM_RITx(RITx));
 
-		// remove divider
-		CLKPWR_SetPCLKDiv(CLKPWR_PCLKSEL_RIT, 1);
-
-        // Get PCLK value of RIT
-    	clock_rate = CLKPWR_GetPCLK(CLKPWR_PCLKSEL_RIT);
-
-        /* calculate compare value for RIT to generate interrupt at
-         * specified time interval
-         * COMPVAL = (RIT_PCLK * time_interval)/1000
-         * (with time_interval unit is millisecond)
-         */
-        cmp_value = (clock_rate /1000000) * microseconds;
-        RITx->RICOMPVAL = cmp_value;
-
-        /* Set timer enable clear bit to clear timer to 0 whenever
-         * counter value equals the contents of RICOMPVAL
-         */
-        RITx->RICTRL |= (1<<1);
-		return cmp_value;
-}
-
-void dump_buf(uint16_t *buf, size_t len) {
-    NVIC_DisableIRQ(RIT_IRQn);
-    size_t i;
-    serial_printf("Buffer dump:\r\nIndex\tValue\r\n");
-    for(i=0; i<len; i++) {
-        serial_printf("%d\t%d\r\n", i, buf[i]);
-    }
-    NVIC_EnableIRQ(RIT_IRQn);
-}
-
-static uint16_t min = 0xFFFF, max = 0;
-static uint32_t sum = 0;
-static uint16_t seq = 0;
-void RIT_IRQHandler(void) {
-	if(RIT_GetIntStatus(LPC_RIT) == SET) {
-        // status_code(0xf);
-    }
-    else {
-        // status_code(1);
-    }
-
+void arc_rithandler(void) {
     buf[seq] = adc_read(ADC_CHANNEL);
-    if(buf[seq] < min) {min = buf[seq];}
-    else if (buf[seq] > max) {max = buf[seq];};
-    sum += buf[seq];
+    
+    if((seq & 0xF) == 0) {
+        digit_writeTwoHexBytes(seq);
+        digit_update();
+    }
 
     if(++seq >= BUF_LEN) {
-        serial_printf("%d samples taken.\r\n\tmin: %d\tmax: %d\r\n\tmean: %d\r\n\n", seq, min, max, sum >> 11);
-        dump_buf(buf, BUF_LEN);
-        min = 0;
-        max = 0;
-        sum = 0;
-        seq = 0;
+        status_code(2);
+        digit_clear();
+        digit_update();
+        NVIC_DisableIRQ(RIT_IRQn);
+        playback_init(buf, BUF_LEN, PLAYBACK_4KHZ);
+        playback_play();
+        /** TODO:
+         * buf should be accepted as uint16_t[]
+         * need to handle that both playback and rec uses RIT interrupt
+         *  + rit.h
+         *      RIT_IRQHandler(void) {
+         *          RIT_GetIntStatus(LPC_RIT);
+         *          rithandler();
+         *      }
+         *      void *rithandler(void)
+         *      setRITHandler(fn) {rithandler = fn;}
+         */
     }
 
 }
