dc1c196967137a41c3f479adb7dd7bfb4c383180
Adam Birtles <ajb665@york.ac.uk>
2019-03-11 10:12:16 +0000

    printer.py: only print datetime after messages ending in a newline

--- pc/printer.py
+++ pc/printer.py
@@ -8,9 +8,14 @@ import serialserver
 try:
     with serialserver.Client(0x01) as serial:
         print('Connected to', serialserver.SOCKET_PATH)
+        newline = True
         while True:
-            time = datetime.now().strftime('%F %T')
             msg = serial.read().decode('ascii')
-            print('[{}] {}'.format(time, msg), end='', flush=True)
+            if newline:
+                time = datetime.now().strftime('%F %T')
+                print('[{}] {}'.format(time, msg), end='', flush=True)
+            else:
+                print(msg, end='', flush=True)
+            newline = msg[-1] == '\n'
 except serialserver.SerialError as e:
     print(e)
